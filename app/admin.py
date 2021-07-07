cda7039f4287d4b14dcd1415dd608c5bb0ed9100
Sergio Aguilar Dinis <sergio.augusto.12@gmail.com>
2021-07-06 22:50:28 -0500

    falta personalizar alumno

---
+++ app/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+from .models import Alumno, Asistencia, Cursos, Periodos, Ingreso_notas
+
+# Register your models here.
+@admin.register(Alumno)
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name','status']
+
+@admin.register(Asistencia)
+class AsistenciaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+@admin.register(Cursos)
+class CursosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status']
+
+
+@admin.register(Periodos)
+class PeriodosAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+
+
+#usuario: sergio123
+#contraseña: idatsdeveloper1
+
+
+@admin.register(Ingreso_notas)
+class IngresonotasAdmin(admin.ModelAdmin):
+    list_display = ['id', 'alumno', 'curso', 'nota', 'periodo']
---
+++ app/models.py
@@ -0,0 +1,93 @@
+from django.db import models
+from django.db.models.fields.files import ImageField
+from django.db.models.query import FlatValuesListIterable
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Alumno(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    status = models.IntegerField(default=1) 
+    
+    class Meta:
+        db_table = 'alumnos'
+        verbose_name = 'Alumno'
+        verbose_name_plural = 'Alumnos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+class Asistencia(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    
+    class Meta:
+        db_table = 'asistencia'
+        verbose_name = 'Asistencia'
+        verbose_name_plural = 'Asistencias'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+class Cursos(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    status = models.IntegerField(default=1)
+
+    class Meta:
+        db_table = 'cursos'
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+# class Notas(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     nota_curso = models.IntegerField()
+
+#     class Meta:
+#         db_table = 'notas'
+#         verbose_name = 'Nota'
+#         verbose_name_plural = 'Notas'
+#         ordering = ['id']
+
+#     def __str__(self):
+#         return self.nota_curso
+
+class Periodos(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255, blank=False, null=False)
+
+    
+    class Meta:
+        db_table = 'periodo'
+        verbose_name = 'Periodo'
+        verbose_name_plural = 'Periodos'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+class Ingreso_notas(models.Model):
+    id = models.AutoField(primary_key=True)
+    alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Cursos, on_delete=models.CASCADE)
+    nota = models.CharField(max_length=2, blank=False, null=False)
+    periodo = models.ForeignKey(Periodos, on_delete=models.CASCADE)
+    content = models.TextField()
+    pub_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'ingresonotas'
+        verbose_name = 'Ingreso Nota'
+        verbose_name_plural = 'Ingreso Notas'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.nota
