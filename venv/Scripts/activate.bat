e1442250439f2c800d5230743098ac25057c2a71
John Olano <john_ol184@hotmail.com>
2021-07-07 09:35:48 -0500

    gitignore

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Retos Diplomado\Reto 13\venv
+set VIRTUAL_ENV=D:\JHOM\idat\repos\hackathon\reto13\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
